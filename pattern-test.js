39ea0b70850137c37a0116470aedb61cf2b3a886
Skiftstar <yukamitft@gmail.com>
2025-08-05 14:13:15 +0200

    adjust pattern testing

---
+++ pattern-test.js
@@ -0,0 +1,8 @@
+const { getPattern } = require("./wordle-calc.js")
+
+console.log(getPattern("aahed", "named"))
+console.log(getPattern("funny", "named"))
+console.log(getPattern("aimed", "named"))
+console.log(getPattern("nanua", "named"))
+console.log(getPattern("nanua", "funny"))
+
--- wordle-calc.js
+++ wordle-calc.js
@@ -1,22 +1,29 @@
+const setCharAt = (str,index,chr) => {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
+
 // Just checks 2 words against each other and what pattern they would produce
 const getPattern = (guess, possibleSolution) => {
   guess = guess.toLowerCase()
   possibleSolution = possibleSolution.toLowerCase()
-  let pattern = ""
+  let pattern = ["b", "b", "b", "b", "b"]
   for (let i = 0; i < possibleSolution.length; i++) {
     if (guess[i] === possibleSolution[i]) {
-      pattern += "g"
-      continue
+      pattern[i] = "g"
+      possibleSolution = setCharAt(possibleSolution, i, " ")
     }
-
-    if (possibleSolution.includes(guess[i])) {
-      pattern += "y"
-      continue
-    }
-
-    pattern += "b"
   }
-  return pattern
+
+  for (let i = 0; i < guess.length; i++) {
+    if (possibleSolution.includes(guess[i])) {
+      pattern[i] = "y"
+      const charIndex = possibleSolution.indexOf(guess[i])
+      possibleSolution = setCharAt(possibleSolution, charIndex, " ")
+    }
+  }
+
+  return pattern.join("")
 }
 
 // Just returns a subset of 'possibleWords' that produce 'pattern' when pitted against 'guess'
